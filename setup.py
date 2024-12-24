cd61532962b748c359c0258c8c71e0a1235f98b2
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-24 14:47:12 +0400

    setup application

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= 'Generative AI Project',
+    version='0.0.1',
+    author='Lakshitha Vimuth',
+    author_email='lvimuthfb@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
